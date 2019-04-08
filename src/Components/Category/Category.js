13fdf38ba2462e6e78c2edcf9f4695480058220e
Peter Yevtushenko <petrucho.u@gmail.com>
2019-04-08 16:36:05 +0300

    Installed react-router-dom, added components Homepage and Category (first work with routers) (April 8) #4

---
+++ src/Components/Category/Category.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Category = () => {
+    return <section>
+            <h1> Category </h1>
+            <a href="/"> Homepage </a>
+            <a href="/category"> Category </a>
+        </section>
+}
+
+export default Category
---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+    return <header>
+            <h1> Homepage </h1>
+            <a href="/"> Homepage </a>
+            <a href="/category"> Category </a>
+        </header>
+}
+
+export default Homepage
