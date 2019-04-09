8b506f0bc7410de40db3fc02f9ba086a492e24d5
Peter Yevtushenko <petrucho.u@gmail.com>
2019-04-09 12:25:15 +0300

    Added axios request and productList (April 9) #3

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import axios from 'axios'
+import Product from '../Product'
 
-const Category = () => {
-    return (
-        <section>
-            <h1> Category </h1>
-            <NavLink to="/"> Homepage </NavLink>
-            <NavLink to="/category"> Category </NavLink>
-        </section>
-    )
+class Category extends React.Component {
+    state = {
+        products: []
+    }
+
+    componentDidMount() {
+        axios.get('https://qa-api.wovenlyrugs.com/products?page=1&page_size=35&size=runners&group=Rug')
+        .then(response => {
+            console.log(response)
+            this.setState({ products: response.data.result.data })
+        })
+    }
+
+    render() {
+
+        const productList = this.state.products.map(product => <Product 
+            title={product.title}
+            description={product.description}
+            key={product.id}
+            image={product.mediaCollection[0].thumbUrl}
+            />)
+
+        return (
+            <section>
+                <h1> Category </h1>
+                <NavLink to="/"> Homepage </NavLink>
+                <NavLink to="/category"> Category </NavLink>
+                { productList }
+            </section>
+        )
+    }
 }
 
 export default Category
