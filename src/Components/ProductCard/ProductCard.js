c6f0a7568ed1f4e998456a09ccf7c3311195f110
Peter Yevtushenko <petrucho.u@gmail.com>
2019-04-12 17:28:10 +0300

    Added oldPrice and deleted unneeded styles (April 12) #7

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -3,6 +3,7 @@ import React from 'react'
 import './ProductCard.css'
 
 const ProductCard = (props) => {
+
     return (
         <div className="productcard">
 			<div className="productcard__image">
@@ -13,7 +14,7 @@ const ProductCard = (props) => {
                     <div> <b>{ props.title }</b> </div>
                 </div>
                 <div className="productcard__price">
-                    <div> { props.price } </div>
+                    <div> <div className="productcard__old-price"> ${Math.round(props.oldPrice / 100)} </div> { props.price } </div>
                 </div>
                 <div className="productcard__colors">                   
                         <div className="colors__border"><span class="colors__dot"></span></div>
