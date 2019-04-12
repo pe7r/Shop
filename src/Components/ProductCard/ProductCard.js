021564bfddbc0dbe377aea7f279a7a14f2a49836
Peter Yevtushenko <petrucho.u@gmail.com>
2019-04-12 12:10:04 +0300

    Changed all <p> and <h1--> on div in PCard and work with size of pc__info (April 12) #1

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -7,15 +7,17 @@ const ProductCard = (props) => {
         <div className="productcard">
 			<div className="productcard__image">
                 <img src={props.image} alt="product" />
-            </div>			
-            <div className="productcard__title">
-                <h4> { props.title } </h4>
             </div>
-            <div className="productcard__price">
-                <p> { props.price } </p>
-            </div>
-            <div className="productcard__colors">
-                <p> Colors here </p>
+            <div className="productcard__info">			
+                <div className="productcard__title">
+                    <div> { props.title } </div>
+                </div>
+                <div className="productcard__price">
+                    <div> { props.price } </div>
+                </div>
+                <div className="productcard__colors">
+                    <div> Colors here </div>
+                </div>
             </div>
 		</div>
     )
