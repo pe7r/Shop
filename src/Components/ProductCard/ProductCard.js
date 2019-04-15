aa1e0f318a82ddac20bdd5495cea432d27c48e87
Peter Yevtushenko <petrucho.u@gmail.com>
2019-04-15 10:43:24 +0300

    Added divs for colors borders (April 15) #2

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -16,13 +16,25 @@ const ProductCard = (props) => {
                 <div className="productcard__price">
                     <div> <div className="productcard__old-price"> ${Math.round(props.oldPrice / 100)} </div> { props.price } </div>
                 </div>
-                <div className="productcard__colors">                   
+                <div className="productcard__colors">
+                    <div className="colors__border">
                         <span class="colors__dot"></span>
+                    </div>
+                    <div className="colors__border">
                         <span class="colors__dot"></span>
+                    </div>
+                    <div className="colors__border">
                         <span class="colors__dot"></span>
+                    </div>
+                    <div className="colors__border">
                         <span class="colors__dot"></span>
+                    </div>
+                    <div className="colors__border">
                         <span class="colors__dot"></span>
+                    </div>
+                    <div className="colors__border">
                         <span class="colors__dot"></span>
+                    </div>
                 </div>
             </div>
 		</div>
