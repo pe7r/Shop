936162412034e1a2a5359a85cc15c20e298d4e88
Peter Yevtushenko <petrucho.u@gmail.com>
2019-04-23 17:59:15 +0300

    Changed placement of API request to save products in App.js state and changed Route to pass props into it (April 23) #9

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 import Homepage from './Components/Homepage/Homepage';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Products from './Components/Products/Products';
 
 class App extends Component {
-  render() {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    axios.get('https://qa-api.wovenlyrugs.com/products?page=1&page_size=12&size=runners&group=Rug')
+    .then(response => {
+        console.log(response)
+        this.setState({ products: response.data.result.data })
+    })
+  }
+
+  render() { 
+    let productsList = this.state.products
+    console.log(productsList)
     return (
       <BrowserRouter>
         <div>
-          <Route exact path='/' component={Homepage} />
-          <Route path='/products' component={Products} />
+          <Route exact path='/' render={() => <Homepage productsList={productsList} />} />
+          <Route path='/products' render={() => <Products productsList={productsList} />} />
         </div>
       </BrowserRouter>
     );
