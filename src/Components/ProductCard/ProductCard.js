097ddc0319b2e02cf72db2f96987b6a196ef8164
Peter Yevtushenko <petrucho.u@gmail.com>
2019-04-27 10:01:17 +0300

    Changed to class component, fixed propTypes, added defaultProps for ProductCard (April 27) #7

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -3,50 +3,56 @@ import PropTypes from 'prop-types'
 
 import './ProductCard.scss'
 
-const ProductCard = (props) => {
-    const imgStyles = {
-        backgroundImage: `url(${props.image})`
+class ProductCard extends React.Component {
+    static defaultProps = {
+        theme: ''
     }
 
-    const { theme, title, oldPrice, price } = props;
+    render() {
+        const { theme, title, oldPrice, price, image } = this.props;
 
-    return (
-        <div className={`productcard ${theme}`}>
-                <div className="productcard__image" style={imgStyles}></div>
-            <div className="productcard__info">			
-                <div className="productcard__title">
-                    <span> <b>{ title }</b> </span>
-                </div>
-                <div className="productcard__price">
-                    <span> 
-                        <span className="productcard__old-price"> ${Math.round(oldPrice / 100)} </span> 
-                        { price } 
-                    </span>
-                </div>
+        const imgStyles = {
+            backgroundImage: `url(${image})`
+        }
 
-                <div className="productcard__colors">
-                    <div className="colors__border--active">
-                        <span className="colors__dot"></span>
+        return (
+            <div className={`productcard ${theme}`}>
+                    <div className="productcard__image" style={imgStyles}></div>
+                <div className="productcard__info">			
+                    <div className="productcard__title">
+                        <span> <b>{ title }</b> </span>
                     </div>
-                    <div className="colors__border">
-                        <span className="colors__dot"></span>
+                    <div className="productcard__price">
+                        <span> 
+                            <span className="productcard__old-price"> ${Math.round(oldPrice / 100)} </span> 
+                            { price } 
+                        </span>
                     </div>
-                    <div className="colors__border">
-                        <span className="colors__dot"></span>
-                    </div>
-                    <div className="colors__border">
-                        <span className="colors__dot"></span>
-                    </div>
-                    <div className="colors__border">
-                        <span className="colors__dot"></span>
-                    </div>
-                    <div className="colors__border">
-                        <span className="colors__dot"></span>
+
+                    <div className="productcard__colors">
+                        <div className="colors__border--active">
+                            <span className="colors__dot"></span>
+                        </div>
+                        <div className="colors__border">
+                            <span className="colors__dot"></span>
+                        </div>
+                        <div className="colors__border">
+                            <span className="colors__dot"></span>
+                        </div>
+                        <div className="colors__border">
+                            <span className="colors__dot"></span>
+                        </div>
+                        <div className="colors__border">
+                            <span className="colors__dot"></span>
+                        </div>
+                        <div className="colors__border">
+                            <span className="colors__dot"></span>
+                        </div>
                     </div>
                 </div>
             </div>
-		</div>
-    )
+        )
+    }
 }
 
 export default ProductCard
