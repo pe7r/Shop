e6fd93f53f870259df354178486acd82be44ca52
Peter Yevtushenko <petrucho.u@gmail.com>
2019-05-02 12:33:14 +0300

    Fixed html structure of homepage (fixed center positioning) (May 2) #3

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -8,19 +8,21 @@ import './Homepage.css'
 const Homepage = (props) => {
     const { productsList } = props;
     return (
-            <section className='homepage__content'>
-                <header>
-                    <h1> Homepage </h1>
-                    <div className="header__links">
-                        <NavLink to="/"> Homepage </NavLink>
-                        <NavLink to="/products"> Products </NavLink>
-                    </div>
-                </header>
-                <div className="homepage__slider-row">
-                    <SliderPopular 
-                        productsList={ productsList }
-                    />
-                </div>                   
+            <section className='homepage__container'>
+                <div className="homepage__content">
+                    <header>
+                        <h1> Homepage </h1>
+                        <div className="header__links">
+                            <NavLink to="/"> Homepage </NavLink>
+                            <NavLink to="/products"> Products </NavLink>
+                        </div>
+                    </header>
+                    <div className="homepage__slider-row">
+                        <SliderPopular 
+                            productsList={ productsList }
+                        />
+                    </div> 
+                </div>                  
             </section>
     )
 }
