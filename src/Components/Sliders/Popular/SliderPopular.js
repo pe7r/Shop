1abb58f57c434d6b4a2c6ea1e213aea12ffb35c3
Peter Yevtushenko <petrucho.u@gmail.com>
2019-05-06 15:07:55 +0300

    Added svg arrows for productcard__buttons (May 6) #11

--- src/Components/Sliders/Popular/SliderPopular.js
+++ src/Components/Sliders/Popular/SliderPopular.js
@@ -15,6 +15,20 @@ class SliderPopular extends React.Component {
     }
 
     render() {
+
+          const leftArrow = <svg width="21" height="12" viewBox="0 0 21 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                d="M0.469669 5.46967C0.176777 5.76256 0.176777 6.23744 0.469669 6.53033L5.24264 11.3033C5.53553 11.5962 6.01041 11.5962 6.3033 11.3033C6.59619 11.0104 6.59619 10.5355 6.3033 10.2426L2.06066 6L6.3033 1.75736C6.59619 1.46447 6.59619 0.989594 6.3033 0.6967C6.01041 0.403807 5.53553 0.403807 5.24264 0.696701L0.469669 5.46967ZM21 5.25L1 5.25L1 6.75L21 6.75L21 5.25Z"
+                                fill="#056551"
+                                />
+                            </svg>
+
+         const rightArrow = <svg width="21" height="12" viewBox="0 0 21 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                d="M20.5303 6.53033C20.8232 6.23744 20.8232 5.76256 20.5303 5.46967L15.7574 0.696699C15.4645 0.403806 14.9896 0.403806 14.6967 0.696699C14.4038 0.989593 14.4038 1.46447 14.6967 1.75736L18.9393 6L14.6967 10.2426C14.4038 10.5355 14.4038 11.0104 14.6967 11.3033C14.9896 11.5962 15.4645 11.5962 15.7574 11.3033L20.5303 6.53033ZM0 6.75L20 6.75V5.25L0 5.25L0 6.75Z"
+                                fill="#056551"
+                                />
+                            </svg>
         const { productsList } = this.props
 
         const productList = productsList.map(product => 
@@ -70,8 +84,8 @@ class SliderPopular extends React.Component {
                             <NavLink to="/products" className="navlink"> See All </NavLink>
                         </div>
                         <div className="slider-popular__buttons">
-                            <button onClick={this.previous}> Prev </button>
-                            <button onClick={this.next}> Next </button>
+                            <button onClick={this.previous}> {leftArrow} </button>
+                            <button onClick={this.next}> {rightArrow} </button>
                         </div>
                     </div>
                 </div>
