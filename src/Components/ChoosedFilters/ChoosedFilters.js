04149fcb7c2a74a50500867e5d22256bd31b981e
Peter Yevtushenko <petrucho.u@gmail.com>
2019-05-29 17:32:36 +0300

    Improving ChooseFilters component (May 29) #9

--- src/Components/ChoosedFilters/ChoosedFilters.js
+++ src/Components/ChoosedFilters/ChoosedFilters.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
+import './ChoosedFilters.scss'
 
 export default class ChoosedFilters extends Component {
     render() {
+        const { showFilterChoice } = this.props;
         const choosedFilters = this.props.choosedFilters.map(filter => {
             return (
-                <button key={filter}>
-                    {filter}
+                <button key={filter} className="choosed__button">
+                    {filter} ✖
                 </button>
             )
         })
         return (
-            <div>
-                {choosedFilters}
+            <div className="choosed__container">
+                {showFilterChoice ? choosedFilters : null}
             </div>
         )
     }
