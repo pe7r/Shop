e70592a6deb3272a5b0a5a115197ace456b12f1b
Peter Yevtushenko <petrucho.u@gmail.com>
2019-05-31 17:45:12 +0300

    Created SearchInput component (May 31) #2

---
+++ src/Components/SearchInput/SearchInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Header from '../Header/Header';
+
+Filt
+export default class SearchInput extends Component {
+    render() {
+        return (
+            <input className="search__input" placeholder="Search"></input>
+        )
+    }
+}
