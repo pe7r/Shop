8b62d091b48d5d0d1d924174f9b4d8b3e2fb712e
Peter Yevtushenko <petrucho.u@gmail.com>
2019-06-05 12:26:45 +0300

    Added closeCross icon (June 5) #1

--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -106,6 +106,11 @@ const moreOptions = <svg width="15" height="13" viewBox="0 0 15 13" fill="none" 
                       <path d="M0 8H15V9H0V8Z" fill="black"/>
                       <path d="M0 12H15V13H0V12Z" fill="black"/>
                     </svg>
+
+const closeCross = <svg width="15" height="15" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd" d="M14 1.4L12.6 0L7 5.6L1.4 0L0 1.4L5.6 7L0 12.6L1.4 14L7 8.4L12.6 14L14 12.6L8.4 7L14 1.4Z" fill="black">
+                      </path>
+                  </svg>
 export { arrowDown,
          leftArrow,
          rightArrow,
@@ -113,4 +118,5 @@ export { arrowDown,
          searchIcon,
          contactIcon,
          cartIcon,
-         moreOptions }
+         moreOptions,
+         closeCross }
