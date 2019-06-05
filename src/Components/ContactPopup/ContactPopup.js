e364e7188d9e6e1c352cc5f4dd80318ceccb16bc
Peter Yevtushenko <petrucho.u@gmail.com>
2019-06-05 18:30:26 +0300

    Started work on OptionsPopup component styles (June 5) #6

--- src/Components/ContactPopup/ContactPopup.js
+++ src/Components/ContactPopup/ContactPopup.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './ContactPopup.scss'
 import {NavLink} from 'react-router-dom'
 
--- src/Components/OptionsPopup/OptionsPopup.js
+++ src/Components/OptionsPopup/OptionsPopup.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './OptionsPopup.scss'
+import {NavLink} from 'react-router-dom'
 
-export default function OptionsPopup() {
+const OptionsPopup = () => {
     return (
-        <div>
-            
+        <div className="options__container">
+            <div className="options__background"></div>
+            <div className="options__main">
+                <h1>Shop Area Rugs</h1>
+                <div className="options__list">
+                    <div className="options__item">
+                        <NavLink>Colors</NavLink>
+                    </div>
+                    <div className="options__item">
+                        <NavLink>Styles</NavLink>
+                    </div>
+                    <div className="options__item">
+                        <NavLink>Sizes</NavLink>
+                    </div>
+                    <div className="options__item">
+                        <NavLink>Rooms</NavLink>
+                    </div> 
+                </div>
+            </div>
         </div>
     )
 }
+
+export default OptionsPopup
