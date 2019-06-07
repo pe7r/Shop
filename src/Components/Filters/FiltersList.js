e094ef71457acc12bfbd065d4ef053fc965518a1
Peter Yevtushenko <petrucho.u@gmail.com>
2019-06-07 10:20:54 +0300

    Added chooseFilter, pushFilters, closeFilter functions for FiltersList

--- src/Components/Filters/FiltersList.js
+++ src/Components/Filters/FiltersList.js
@@ -9,7 +9,14 @@ export default class FiltersList extends Component {
     setFilter: '',
     choosedFilters: [],
     choosed: false,
-    showFilterChoice: false
+    showFilterChoice: false,
+    size: [],
+    color: [],
+    room: [],
+    price: [],
+    material: [],
+    construction: [],
+    style: []
   }
 
   chooseFilter = (id) => {
@@ -18,46 +25,26 @@ export default class FiltersList extends Component {
     })
   }
 
-  setChoice = () => {
+  pushFilters = (arr, filter) => {
     this.setState({
-      showFilterChoice: true
+      [filter]: arr
     })
   }
 
-  handleBoxChoose = (name) => {
-    const {choosedFilters} = this.state;
-    let newList = choosedFilters;
-     if (choosedFilters.includes(name)){
-      const idx = newList.findIndex(item => item === name);
-      newList = [
-        ...choosedFilters.slice(0, idx),
-        ...choosedFilters.slice(idx + 1)
-      ]
-    } else if (!choosedFilters.includes(name)) {
-      newList.push(name)
-    }
-    console.log(newList)
-
-    this.setState({
-      choosedFilters: newList
-    })
-  }
-
-  applyChanges = () => {
-    const { choosedFilters } = this.state;
-    this.setChoice()
+  closeFilter = () => {
     this.setState({
       setFilter: ''
     })
-    if (choosedFilters.length < 1) {
-      this.setState({
-        showFilterChoice: false
-      })
-    }
   }
 
   render() {
-    const {boxShow, setFilter, choosedFilters, showFilterChoice} = this.state;
+    const {
+      boxShow,
+      setFilter,
+      choosedFilters,
+      showFilterChoice
+    } = this.state;
+
     const buttons = Object.keys(filtersData).map((key) => {
       let active = '';
       if (key === setFilter) {
@@ -69,8 +56,8 @@ export default class FiltersList extends Component {
                      chooseFilter={this.chooseFilter}
                      boxShow={boxShow}
                      active={active}
-                     handleBoxChoose={this.handleBoxChoose}
-                     applyChanges={this.applyChanges}
+                     closeFilter={this.closeFilter}
+                     pushFilters={this.pushFilters}
                      />
     });
 
