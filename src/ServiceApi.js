e45392eddaafa329ab46cf35b22ac26fe84bc2a9
Peter Yevtushenko <petrucho.u@gmail.com>
2019-06-10 16:58:34 +0300

    Added processing string to API request for Filters

--- src/ServiceApi.js
+++ src/ServiceApi.js
@@ -1,9 +1,31 @@
 import axios from 'axios'
 
 export default class ServiceApi {
-    static async getProductsList(actualPage) {
+    static async getProductsList(actualPage, sizes = [], colors = []) {
+        console.log('sizes', sizes)
+        const sizeFilters = sizes.map(size => {
+            let sizeToString;
+            
+            if (sizes[0] === size) {
+                sizeToString = `&size=${size}`
+            } else {
+                sizeToString = `%2C${size}`
+            }
+            return sizeToString
+        })
+
+        const colorFilters = colors.map(color => {
+            let colorToString;
+            
+            if (colors[0] === color) {
+                colorToString = `&color=${color}`
+            } else {
+                colorToString = `%2C${color}`
+            }
+            return colorToString
+        })
         const res = await axios.get(
-            `https://qa-api.wovenlyrugs.com/products?page=${actualPage}&page_size=12&size=runners&group=Rug`
+            `https://qa-api.wovenlyrugs.com/products?page=${actualPage}&page_size=12${sizeFilters.join('')}${colorFilters.join('')}&group=Rug`
             )
         return res;
     }
