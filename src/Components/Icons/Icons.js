70c2a9b9f5cebcf0d3b6558bc31c894174693d67
Peter Yevtushenko <petrucho.u@gmail.com>
2019-06-12 16:56:02 +0300

    Added icon

--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -116,6 +116,8 @@ const closeCrossBlack = <svg xmlns="http://www.w3.org/2000/svg" height="15" widt
 
 const sortArrowDown = <svg xmlns="http://www.w3.org/2000/svg" height="15" width="15" viewBox="0 0 320 512"><path d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"></path></svg>
 
+const sortArrowDownActive = <svg xmlns="http://www.w3.org/2000/svg" height="15" width="15" viewBox="0 0 320 512"><path d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"></path></svg>
+
 export { arrowDown,
          leftArrow,
          rightArrow,
@@ -126,4 +128,5 @@ export { arrowDown,
          moreOptions,
          closeCross,
          closeCrossBlack,
-         sortArrowDown }
+         sortArrowDown,
+         sortArrowDownActive }
