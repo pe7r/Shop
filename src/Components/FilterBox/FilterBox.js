7b88de23e94bfe93dfcddbf34eb8d943c69eb55b
Peter Yevtushenko <petrucho.u@gmail.com>
2019-06-14 12:51:26 +0300

    Changed FilterBox to functional component

--- src/Components/FilterBox/FilterBox.js
+++ src/Components/FilterBox/FilterBox.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './FilterBox.scss'
 
-export default class FilterBox extends Component {
-    render() {
-        const {
-          filter,
-          handleBoxChoice,
-          boxActive
-        } = this.props;
-
-        return (
-            <div>
-              <button
-                      className={`box__button ${boxActive}`}
-                      onClick={
-                        () => handleBoxChoice(filter.id)
-                      }
-                      > {filter.title}
-              </button>
-            </div>
-        )
-    }
+const FilterBox = ({filter, handleBoxChoice, boxActive}) => {
+  return (
+      <div>
+        <button
+                className={`box__button ${boxActive}`}
+                onClick={
+                  () => handleBoxChoice(filter.id)
+                }
+                > {filter.title}
+        </button>
+      </div>
+  )
 }
+
+export default FilterBox
