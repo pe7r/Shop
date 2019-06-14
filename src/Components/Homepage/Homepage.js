8f2dcf0a3db9fb7f5e8991f4147340f8de2955cd
Peter Yevtushenko <petrucho.u@gmail.com>
2019-06-14 12:57:58 +0300

    Deleted unneeded indent

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -28,8 +28,7 @@ const Homepage = (props) => {
                             />
                         </div>
                     </div>      
-                }
-                            
+                }                
             </section>
     )
 }
