ad9246bb7394dca75e9511efe8f0596bdfb490c6
Peter Yevtushenko <petrucho.u@gmail.com>
2019-06-19 16:34:34 +0300

    Created e2e test with puppeteer

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,38 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+const puppeteer = require('puppeteer');
+
+describe('Products title', () => {
+  test('Title loads correctly', async () => {
+	let browser = await puppeteer.launch({
+	  headless: false
+	});
+	let page = await browser.newPage();
+
+	page.emulate({
+	  viewport: {
+		width: 1200,
+		height: 600
+	  },
+	  userAgent: ''
+	});
+
+  await page.goto('http://localhost:3000/')
+  await page.click('a[name=shopAllRugs]')
+  await page.waitForSelector('.products__content')
+
+  // const htmlCard = await page.$eval('.productcard__title Brynn', e => e.innerHTML);
+  // expect(htmlCard).toBe('Brynn');
+  
+  await page.click('button[name=size]')
+  await page.click('button[name=runners]')
+  await page.click('button[class=box__button-apply]')
+
+  await page.waitForSelector('.products__content')
+
+	browser.close();
+  }, 50000);
+});
