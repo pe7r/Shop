3e91fd7c19f475eb6cc768c5d5f6ecf89ca8d323
Peter Yevtushenko <petrucho.u@gmail.com>
2019-06-19 17:33:19 +0300

    Added test that checks if ProductCard #3 was changed after filter choice size: runners

--- src/App.test.js
+++ src/App.test.js
@@ -26,18 +26,28 @@ describe('Products title', () => {
 	  userAgent: ''
 	});
 
-  await page.goto('http://localhost:3000/')
-  await page.click('a[name=shopAllRugs]')
-  await page.waitForSelector('.products__content')
+  await page.goto('http://localhost:3000/');
+  await page.click('a[name=shopAllRugs]');
+  await page.waitForSelector('.products__content');
+  console.log(page._events);
 
-  // const htmlCard = await page.$eval('.productcard__title Brynn', e => e.innerHTML);
-  // expect(htmlCard).toBe('Brynn');
+  const firstCard = await page.$eval('#product0', e => e.innerHTML);
+  expect(firstCard).toBe(' Brynn ');
   
-  await page.click('button[name=size]')
-  await page.click('button[name=runners]')
-  await page.click('button[class=box__button-apply]')
+  const thirdCard = await page.$eval('#product2', e => e.innerHTML);
+  expect(thirdCard).toBe(' Shiv ');
+  
+  await page.click('button[name=size]');
+  await page.click('button[name=runners]');
+  await page.click('button[class=box__button-apply]');
 
-  await page.waitForSelector('.products__content')
+  await page.waitForSelector('.products__content');
+
+  const firstCardRunner = await page.$eval('#product0', e => e.innerHTML);
+  expect(firstCardRunner).toBe(' Brynn ');
+  
+  const thirdCardRunner = await page.$eval('#product2', e => e.innerHTML);
+  expect(thirdCardRunner).toBe(' Amela ');
 
 	browser.close();
   }, 50000);
